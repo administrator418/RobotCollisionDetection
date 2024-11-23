b65906a7b717c81daca0de7c199352c2e68f3581
Jayden <administrator418@users.noreply.github.com>
2024-11-23 12:37:36 +0800

    3

--- test.py
+++ test.py
@@ -4,3 +4,4 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s-%(levelname)s-%(message)s',
                     handlers=[logging.StreamHandler()])
 logging.info('Hello World')
+print('Hello')
